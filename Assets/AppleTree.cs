998736b058d906271fb7a8424d065111809a3b72
emmanuelsalas <eas282@scarletmail.rutgers.edu>
2016-10-06 17:15:14 -0400

    Update 2
    Apples falling from sides, layers added.

--- Assets/AppleTree.cs
+++ Assets/AppleTree.cs
@@ -9,7 +9,7 @@ public class AppleTree : MonoBehaviour {
 	public float speed=1f;
 
 	//Distance where AppleTree turns around
-	public float leftAndRightEdge = 10f;
+	public float leftAndRightEdge = 20f;
 
 	//Chance that the AppleTree will change directions
 	public float chanceToChangeDirections = 0.1f;
@@ -19,13 +19,31 @@ public class AppleTree : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		//Droppeing apples every second
-	
+		//Dropping apples every second
+		InvokeRepeating ("DropApple", 2f, secondsBetweenAppleDrops);
 	}
+		void DropApple(){
+		GameObject apple = Instantiate (applePreFab) as GameObject;
+				apple.transform.position = transform.position;
+		}
 	
 	// Update is called once per frame
 	void Update () {
 		//Basic movement
+		Vector3 pos = transform.position;
+		pos.x += speed * Time.deltaTime;
+		transform.position = pos;
+
 		//Changing Direction
+		if (pos.x < -leftAndRightEdge) {
+			speed = Mathf.Abs (speed); //Move Right
+		} else if (pos.x > leftAndRightEdge) {
+			speed = -Mathf.Abs (speed); //Move left
+		}
+	}
+	void FixedUpdate() {
+		if (Random.value < chanceToChangeDirections) {
+			speed *= -1; //Change Direction
+		}
 	}
 }
