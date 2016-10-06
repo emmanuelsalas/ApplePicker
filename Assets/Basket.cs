15f706f59ea7b6415e984d0588907426872f5fa3
emmanuelsalas <eas282@scarletmail.rutgers.edu>
2016-10-06 18:30:14 -0400

    FinalV1
    Final with errors, specifically in the GUI Text and Mouse not moving
    around.

--- Assets/Basket.cs
+++ Assets/Basket.cs
@@ -3,9 +3,10 @@ using System.Collections;
 
 public class Basket : MonoBehaviour {
 	
+	public UIText scoreGT;
+
 	// Update is called once per frame
 	void Update () {
-	
 		//Get the current screen position of the mouse from Input
 		Vector3 mousePos2D = Input.mousePosition; //1
 
@@ -21,11 +22,27 @@ public class Basket : MonoBehaviour {
 		this.transform.position = pos;
 	}
 
+	void Start(){
+		//Find a reference to the ScoreCounter GameObject
+		GameObject scoreGO = GameObject.Find("ScoreCounter");
+		//Get the GUIText Component of that GameObject
+		scoreGT = scoreGO.GetComponent<UIText>();
+		//Set the starting number of points to 0
+		scoreGT.text = "0";
+	}
+
 	void OnCollisionEnter (Collision coll){
 		//Find out what hit this basket
 		GameObject collidedWith = coll.gameObject;
 		if (collidedWith.tag == "Apple") {
 			Destroy (collidedWith);
 		}
+
+		//Parse the text of the scoreGT into an int
+		int score = int.Parse(scoreGT.text);
+		//Add points for catching the apple
+		score += 100;
+		//Convert the score back to a string and display it
+		scoreGT.text = score.ToString();
 	}
 }
