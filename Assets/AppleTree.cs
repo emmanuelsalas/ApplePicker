407d67eedf117b9a6cb1fb40588f4c4b9d41ed9c
emmanuelsalas <eas282@scarletmail.rutgers.edu>
2016-10-13 00:47:30 -0400

    Test1
    Blah

--- Assets/AppleTree.cs
+++ Assets/AppleTree.cs
@@ -2,48 +2,48 @@
 using System.Collections;
 
 public class AppleTree : MonoBehaviour {
-	//Prefab for insantiating apples
-	public GameObject applePreFab;
+	//Prefab for instantiating apples
+	public GameObject applePrefab;
 
-	//Speed at which the AppleTree moves in meters/seconds
+	//Speed at which the AppleTree moves in meters/second
 	public float speed=1f;
 
 	//Distance where AppleTree turns around
-	public float leftAndRightEdge = 20f;
+	public float leftAndRightEdge = 10f;
 
 	//Chance that the AppleTree will change directions
-	public float chanceToChangeDirections = 0.1f;
+	public float chanceToChangeDirections=0.1f;
 
 	//Rate at which Apples will be instantiated
 	public float secondsBetweenAppleDrops = 1f;
 
-	// Use this for initialization
-	void Start () {
-		//Dropping apples every second
-		InvokeRepeating ("DropApple", 2f, secondsBetweenAppleDrops);
+	void start(){
+		//Dropping apple every second
+		InvokeRepeating("DropApple", 2f, secondsBetweenAppleDrops);
 	}
-		void DropApple(){
-		GameObject apple = Instantiate (applePreFab) as GameObject;
-				apple.transform.position = transform.position;
-		}
-	
-	// Update is called once per frame
-	void Update () {
-		//Basic movement
+
+	void DropApple(){
+		GameObject apple = Instantiate (applePrefab) as GameObject;
+		apple.transform.position = transform.position;
+	}
+
+	void Update (){
+		//Basic Movement
 		Vector3 pos = transform.position;
 		pos.x += speed * Time.deltaTime;
 		transform.position = pos;
-
 		//Changing Direction
 		if (pos.x < -leftAndRightEdge) {
-			speed = Mathf.Abs (speed); //Move Right
+			speed = Mathf.Abs (speed); //MoveRight
 		} else if (pos.x > leftAndRightEdge) {
 			speed = -Mathf.Abs (speed); //Move left
 		}
 	}
-	void FixedUpdate() {
+	void FixedUpdate ()
+	{
+		//Changing Direction Randomly
 		if (Random.value < chanceToChangeDirections) {
-			speed *= -1; //Change Direction
+			speed *= -1;//Change Direction
 		}
 	}
 }
